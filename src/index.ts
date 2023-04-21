1a329f651024f62caad8b66d54865e204e9195f9
Claudio Saccella <cdsaccella@gmail.com>
2023-04-21 00:25:27 -0300

    add tests

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import express from 'express'
+import app from './app'
 
-const app = express()
-
-app.use(express.json())
 const PORT = 3000
-
-app.get('/hello', (_, res) => {
-  console.log('hi!')
-  res.send('how are you?')
-})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
